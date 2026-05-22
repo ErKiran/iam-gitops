fe15b8f3ad37365353879908fb97c579720d6c03
Kiran Adhikari <kiran@ochrebyte.com>
2026-05-22 17:43:21 -0500

    IAMINFRA-2 fixed the group naming rules

--- groups.tf
+++ groups.tf
@@ -1,4 +1,4 @@
 resource "okta_group" "neobank_finance_users" {
-  name        = "neobank_finance_users"
+  name        = "Apps-neobank_finance_users"
   description = "Groups exclusive for finance users of neobank"
 }
