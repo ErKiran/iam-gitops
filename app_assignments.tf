25fb333fe31d609aaa7853a7e9d3426392f3eb9c
Kiran Adhikari <kiran@ochrebyte.com>
2026-05-22 21:03:56 -0500

    IAMINFRA-4 add app resources for drift detection

--- app_assignments.tf
+++ app_assignments.tf
@@ -1,5 +1,5 @@
 resource "okta_app_group_assignments" "tellers_to_neobank_gitops" {
-  app_id   = okta_app_saml.neobank_gitOps.id
+  app_id = okta_app_saml.neobank_gitOps.id
   group {
     id = okta_group.neobank_teller_group.id
   }
