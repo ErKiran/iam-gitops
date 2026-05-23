ee87da83f66e058a09b0ed5df0d918ea28019920
Kiran Adhikari <kiran@ochrebyte.com>
2026-05-22 19:20:02 -0500

    IAMINFRA-3: Create a groups for the it admin IAM users

--- app_assignments.tf
+++ app_assignments.tf
@@ -1,8 +1,3 @@
-resource "okta_app_group_assignment" "finance_users_to_neobank" {
-  app_id   = data.okta_app.neobank.id
-  group_id = okta_group.neobank_finance_users.id
-}
-
 resource "okta_group_rule" "neobank_iam_admin_users_rule" {
   name              = "Assign IAM Admin to Neobank"
   status            = "ACTIVE"
