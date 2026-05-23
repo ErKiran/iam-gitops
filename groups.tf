125b78603891b21811139de4923405545bbdf898
Kiran Adhikari <kiran@ochrebyte.com>
2026-05-22 19:15:33 -0500

    IAMINFRA-3: Create a groups for the it admin IAM users

--- groups.tf
+++ groups.tf
@@ -1,9 +1,4 @@
-resource "okta_group" "neobank_finance_users" {
-  name        = "Apps-neobank_finance_users"
-  description = "Groups exclusive for finance users of neobank"
-}
-
-resource "okta_group" "neobank_iam_users" {
-  name        = "Apps-neobank_iam_users"
+resource "okta_group" "neobank__it_iam_users" {
+  name        = "Apps-neobank__it_iam_users"
   description = "Groups exclusive for IT Admins of neobank"
 }
